948d5fdd25425259a2ec77627d21ed3a2ce3173a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-06-13 17:29:42 -0700

    Use redirect for form post and flash message, rather than render new html.

--- hackbright_web.py
+++ hackbright_web.py
@@ -1,10 +1,11 @@
 """A web application for tracking projects, students, and student grades."""
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, flash
 
 import hackbright
 
 app = Flask(__name__)
+app.secret_key = "secretsecretsecret"
 
 
 @app.route("/student")
@@ -15,12 +16,10 @@ def get_student():
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    html = render_template("student_info.html",
+    return render_template("student_info.html",
                            first=first,
                            last=last,
                            github=github)
-
-    return html
 
 
 @app.route("/student-search")
@@ -37,7 +36,7 @@ def student_add():
     return render_template("student_add.html")
 
 
-@app.route("/student-add-success", methods=['POST'])
+@app.route("/student-add", methods=['POST'])
 def confirm_student_added():
     """Confirm that student has been added."""
 
@@ -46,14 +45,13 @@ def confirm_student_added():
     github = request.form.get('github')
 
     success = hackbright.make_new_student(first_name, last_name, github)
+    flash(success)
 
-    html = render_template("student_add_success.html",
-                           first_name=first_name,
-                           last_name=last_name,
-                           github=github,
-                           success=success)
-
-    return html
+    return redirect("/student?github=" + github)
+                    # first_name=first_name,
+                    # last_name=last_name,
+                    # github=github,
+                    # success=success)
 
 
 if __name__ == "__main__":
